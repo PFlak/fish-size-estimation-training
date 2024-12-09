186aae721a5b437cc8cb0867f98466659e91772d
PFlak <piotr.andrzej.flak@gmail.com>
2024-12-09 11:56:07 +0100

    fishSizeEstimation Dataset

--- fishSizeEstimation/fishSizeEstimation.py
+++ fishSizeEstimation/fishSizeEstimation.py
@@ -1,6 +1,6 @@
 """
 Mask R-CNN
-Train on the toy Balloon dataset and implement color splash effect.
+Train on the toy FishSizeEstimation dataset and implement color splash effect.
 
 Copyright (c) 2018 Matterport, Inc.
 Licensed under the MIT License (see LICENSE for details)
@@ -12,13 +12,13 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
        the command line as such:
 
     # Train a new model starting from pre-trained COCO weights
-    python3 fishSizeEstimation.py train --dataset=/path/to/balloon/dataset --weights=coco
+    python3 fishSizeEstimation.py train --dataset=/path/to/fishSizeEstimation/dataset --weights=coco
 
     # Resume training a model that you had trained earlier
-    python3 fishSizeEstimation.py train --dataset=/path/to/balloon/dataset --weights=last
+    python3 fishSizeEstimation.py train --dataset=/path/to/fishSizeEstimation/dataset --weights=last
 
     # Train a new model starting from ImageNet weights
-    python3 fishSizeEstimation.py train --dataset=/path/to/balloon/dataset --weights=imagenet
+    python3 fishSizeEstimation.py train --dataset=/path/to/fishSizeEstimation/dataset --weights=imagenet
 
     # Apply color splash to an image
     python3 fishSizeEstimation.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
@@ -76,3 +76,113 @@ class FishSizeEstimationConfig(Config):
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
+
+############################################################
+#  Dataset
+############################################################
+
+class FishSizeEstimationDataset(utils.Dataset):
+
+
+    def load_fishsizeestimation(self, dataset_dir, subset):
+        """Load a subset of the fishSizeEstimation dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("fse", 1, "Chelidonichthys lastoviza")
+        self.add_class("fse", 2, "Diplodus vulgaris")
+        self.add_class("fse", 3, "Pagrus pagrus")
+        self.add_class("fse", 4, "Scomber japonicus")
+        self.add_class("fse", 5, "Scorpaena notata")
+        self.add_class("fse", 6, "Scorpaena porcus")
+        self.add_class("fse", 7, "Serranus scriba")
+        self.add_class("fse", 8, "Species 10")
+        self.add_class("fse", 9, "Species 11")
+        self.add_class("fse", 10, "Species 13")
+        self.add_class("fse", 11, "Species 15")
+        self.add_class("fse", 12, "Species 18")
+        self.add_class("fse", 13, "Species 20")
+        self.add_class("fse", 14, "Species 22")
+        self.add_class("fse", 15, "Species 3")
+        self.add_class("fse", 16, "Species 32")
+        self.add_class("fse", 17, "Species 36")
+        self.add_class("fse", 18, "Species 38")
+        self.add_class("fse", 19, "Species 4")
+        self.add_class("fse", 20, "Species 46")
+        self.add_class("fse", 21, "Species 6")
+        self.add_class("fse", 22, "Species 68")
+        self.add_class("fse", 23, "Species 7")
+        self.add_class("fse", 24, "Species 81")
+        self.add_class("fse", 25, "Species 84")
+        self.add_class("fse", 26, "Species 85")
+        self.add_class("fse", 27, "Species 86")
+        self.add_class("fse", 28, "Species 87")
+        self.add_class("fse", 29, "Sphyraena sphyraena")
+        self.add_class("fse", 30, "Spicara smaris")
+        self.add_class("fse", 31, "Trachurus spp")
+
+
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        annotations = json.load(open(os.path.join(dataset_dir, "annotations.json")))
+        annotations = annotations["files"]
+
+        for annotation in annotations:
+            path = os.path.join(dataset_dir, annotation["filename"])
+            height = annotation["height"]
+            width = annotation["width"]
+            regions = annotation["regions"]
+
+            self.add_image(
+                "fse",
+                image_id=annotation["filename"],
+                path=path,
+                width=width,
+                height=height,
+                regions=regions
+            )
+
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a fishSizeEstimation dataset image, delegate to parent class.
+        # image_info = list(filter(lambda x: x['id'] == image_id, self.image_info))[0]
+        image_info = self.image_info[image_id]
+        # print(image_info)
+        if image_info["source"] != "fse":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = image_info
+        mask = np.zeros([info["height"], info["width"], len(info["regions"])],
+                        dtype=np.uint8)
+        classes = []
+        for i, p in enumerate(info["regions"]):
+            classes.append(p["class_id"])
+            # Get indexes of pixels inside the polygon and set them to 1
+            y = [pt[1] for pt in p["polygons"]]
+            x = [pt[0] for pt in p["polygons"]]
+            rr, cc = skimage.draw.polygon(y, x)
+            mask[rr, cc, i] = 1
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        return mask.astype(np.bool), np.array(classes).astype(np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "fse":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
